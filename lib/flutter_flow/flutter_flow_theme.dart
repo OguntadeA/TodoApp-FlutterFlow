ea9b6529176ab46ded13b102e0550c380779231f
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-02-24 19:22:39 +0000

    Updating to latest FlutterFlow output.

--- lib/flutter_flow/flutter_flow_theme.dart
+++ lib/flutter_flow/flutter_flow_theme.dart
@@ -227,14 +227,14 @@ class ThemeTypography extends Typography {
   String get labelLargeFamily => 'Inter';
   TextStyle get labelLarge => GoogleFonts.getFont(
         'Inter',
-        color: const Color(0x4014181B),
+        color: Color(0x4014181B),
         fontWeight: FontWeight.normal,
         fontSize: 16.0,
       );
   String get labelMediumFamily => 'Inter';
   TextStyle get labelMedium => GoogleFonts.getFont(
         'Inter',
-        color: const Color(0x4C14181B),
+        color: Color(0x4C14181B),
         fontWeight: FontWeight.normal,
         fontSize: 14.0,
       );
